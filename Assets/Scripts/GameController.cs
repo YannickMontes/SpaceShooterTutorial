4bad99b54076e984aea4f6ff2cf9cf006425debf
YannickMontes <montes.yannick@gmail.com>
2018-02-08 15:06:22 +0100

    Waves & destroy explosion

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,16 +6,29 @@ public class GameController : MonoBehaviour
 {
     public GameObject hazard;
     public Vector3 spawnValues;
-
-    private void SpawnWaves()
-    {
-        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-        Quaternion spawnRotation = Quaternion.identity;
-        Instantiate(hazard, spawnPosition, spawnRotation);
-    }
+    public float timeBeforeWavesStart;
+    public float spawnHazardWait;
+    public float wavesWait;
 
     private void Start()
     {
-        SpawnWaves();
+        StartCoroutine(SpawnWaves());
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(timeBeforeWavesStart);
+        while (true)
+        {
+            int hazardCount = Random.Range(5, 15);
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnHazardWait);
+            }
+            yield return new WaitForSeconds(wavesWait);
+        }
     }
 }
