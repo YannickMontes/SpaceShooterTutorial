db560a67a990fe138e4975370f07f0ebc3dd12c7
YannickMontes <montes.yannick@gmail.com>
2018-02-08 14:35:28 +0100

    Start implement an asteroid

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (IsBoundary(other.tag))
+        {
+            return;
+        }
+        Destroy(other.gameObject);
+        Destroy(this.gameObject);
+    }
+
+    private bool IsBoundary(string tag)
+    {
+        return tag == "Boundary";
+    }
+}
