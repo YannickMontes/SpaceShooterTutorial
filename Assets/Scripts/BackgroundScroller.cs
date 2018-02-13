5ed686adb246d1b90920ccb38ac338a2b7843bb8
YannickMontes <montes.yannick@gmail.com>
2018-02-13 20:08:28 +0100

    Background scroll

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour {
+
+    public float scroolSpeed;
+    public float tileSizeZ;
+
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+        Debug.Log(startPosition);
+    }
+
+    private void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scroolSpeed, tileSizeZ);
+        transform.position = startPosition + Vector3.forward * newPosition;
+    }
+}
